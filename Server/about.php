64be04cf7e307238acbcf78f543b4cd97fb27a70
BI4HUU <BI4HUU@yandex.ru>
2019-11-30 09:21:57 +0200

    Add files via upload

--- Server/about.php
+++ Server/about.php
@@ -1,4 +1,4 @@
 <?php include 'header.php'?>
-<style>.nav_item:nth-child(2){text-decoration:underline}</style>
-<section class="container page2"><h2 class="text-center">О нас</h2><p>&nbsp;&nbsp;&nbsp;&nbsp;Applerepairss— это сервисный центр по ремонту техники Apple. Мы работаем с 2014 года. Мы были одними из первых, кто осуществляет ремонт на выезде. <br><br>&nbsp;&nbsp;&nbsp;&nbsp;На сегодняшний день мы являемся лидерами на рынке и популярности.</p></section>
+<section class="container page2"><h2 class="text-center">О нас</h2><p>&nbsp;&nbsp;&nbsp;&nbsp;Applerepairss— это сервисный центр по ремонту техники Apple. Мы работаем по Москве и области. Мы не тратим денег на дорогой маркетинг и рекламу, поэтому мы оказываем все услуги по приемлемой цене и даем ГАРАНТИЮ. У нас в штате 13 мастеров, с опытом работы минимум 3 года. <br><br>&nbsp;&nbsp;&nbsp;&nbsp;На сегодняшний день мы являемся стремительно растущей компанией и поставили перед собой цель сделать 100.000 ремонтов смартфонов. Помогите нам сделать эту задачу - <span class="href" onclick="popout_see()">оставьте заявку.</span></p></section>
+<?php include 'popout.php'?>
 <?php include 'footer.php'?>
--- Server/mail.php
+++ Server/mail.php
@@ -1,10 +1,20 @@
-/getUpdates
+
 $to = "petr.pawlikov@yandex.ru";
-<?php
 $to = "be4huu@gmail.com";
-    $subject = "******";
-    $message = implode (" | ", $_POST );
+		file_get_contents('https://api.telegram.org/bot808970621:AAEaA0jzGTzo5yKb4AQT0Pf-Pm3n-eEt63U/sendMessage?chat_id=666543398&text=' . $message)
     $headers = "From: info@sviter.kz\r\nReply-to: info@sviter.kz\r\nContent-type: text/plain; charset=utf-8\r\n";
     mail($to, $subject, $message, $headers);
-		file_get_contents('https://api.telegram.org/bot808970621:AAEaA0jzGTzo5yKb4AQT0Pf-Pm3n-eEt63U/sendMessage?chat_id=666543398&text=888' . $message)
+1) В телеграм в поиске найдите @BotFather (с галочкой в конце). 
+2) нажмите старт 
+3) напишите /newbot . 
+4) напишите имя *** (или как хотите). 
+5) напишите имя ***_bot (в конце _bot обязательно!). 
+6) пришлите мне код который получите типа (808970621:AAEaA0jzGTzo5yKb4AQT0Pf-Pm3n-eEt63U)
+7) В телеграм в поиске найдите бота по имени из пункта 5 и напишите чтото ему.
+https://api.telegram.org/bot808970621:AAEaA0jzGTzo5yKb4AQT0Pf-Pm3n-eEt63U/getUpdates
+<?php
+$to = "petr.pawlikov@yandex.ru";
+    $subject = "******";
+    $message = implode (" | ", $_POST );
+    mail($to, $subject, $message);
 ?>
